4bebdb9cf8221b7caaf75d66e4e6d30ae38fb9c3
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-09-17 02:43:25 -0400

    update

--- assignment1/problem_4.py
+++ assignment1/problem_4.py
@@ -1,13 +1,30 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+import scipy.constants as con
 
-data = np.loadtxt("lakeshore.txt") 
+e0 = con.epsilon_0
+d = np.linspace(0,10,100)
+#We can always align our axis in such a way that z represents the distance from the shell.
+#Let's use the function given by Griffiths Problem 2.7; we can use the substituted integral with respect to u.
+#However, we can write the function in terms of z/R and therefore, the distance z can be written in terms of R.
+#We can also write the field in terms of sigma, i.e. E/sigma where sigma is the charge per unit area (surface charge density).
+def E(u,z):
+    coeff = 1/(2*e0)
+    integrand = (z-u)/((1+z**2-2*z*u)**(3/2))
+    return coeff*integrand
+
+#Integrator
+
+
+#scipy.quad
+Eq = []
+for z in d:
+    Eq.append(integrate.quad(E,-1,1,args=(z))[0])
+plt.plot(d,Eq,label="scipy.quad")
+plt.legend()
+plt.show()
 
 """
-Now that we have the data imported, we want to interpolate to find a temperature from a voltage.
-We are given dV/dT (in mV/K) so we can simply linearly interpolate each time, assuming that the the
-function is linear between respective voltages, being careful to convert mV to V. 
-We make the assumption that since we are interpolating, there cannot be voltages smaller or larger than what
-we have been given.
+There is a singularity in the integral
 """
-
-
