bf8d3f849d43f6779d4b96516137ddf57a8505cb
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-09-17 00:39:34 -0400

    Update 2

---
+++ assignment1/problem_3/lorentzian.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from ratfit_exact import rat_fit, rat_eval
+
+x = np.linspace(-np.pi/2,np.pi/2,100)
+y = np.cos(x)
+xp = np.linspace(-np.pi/2,np.pi/2,10)
+yp = np.cos(xp)
+plt.plot(xp,yp, "x")
+plt.plot(x,y,label="cos(x)")
+
+#Polynomial interpolation (we shall use 10 points for each interpolation i.e. a polynomial of order n-1=9)
+ord = 9
+coeffs = np.polyfit(x,y,deg=ord)
+ypoly = np.polyval(coeffs, x)
+coeffsp= np.polyfit(xp,yp,deg=ord)
+yppoly = np.polyval(coeffsp, xp)
+plt.plot(x,ypoly,label="Polynomial")
+print("Polynomial Std=",np.std(ypoly-y, ddof=1))
+
+#Cubic Spline
+spln = interpolate.splrep(xp,yp)
+yspln = interpolate.splev(x,spln)
+plt.plot(x,yspln,label="Cubic Spline")
+print("Cubic Spline Std=",np.std(yspln-y, ddof=1))
+
+#Rational Function (using the functions from lectures)
+n,m = 5,6 #as the function is concave down, m>n.
+p,q = rat_fit(xp,yp,n,m)
+pred = rat_eval(p,q,xp)
+yrat = rat_eval(p,q,x)
+plt.plot(x, yrat, label="Rational")
+print("Rational Std=", np.std(yrat-y, ddof=1))
+
+plt.legend()
+plt.show()
---
+++ assignment1/problem_3/ratfit_exact.py
@@ -0,0 +1,26 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def rat_eval(p,q,x):
+    top=0
+    for i in range(len(p)):
+        top=top+p[i]*x**i
+    bot=1
+    for i in range(len(q)):
+        bot=bot+q[i]*x**(i+1)
+    return top/bot
+
+def rat_fit(x,y,n,m):
+    assert(len(x)==n+m-1)
+    assert(len(y)==len(x))
+    mat=np.zeros([n+m-1,n+m-1])
+    for i in range(n):
+        mat[:,i]=x**i
+    for i in range(1,m):
+        mat[:,i-1+n]=-y*x**i
+    pars=np.dot(np.linalg.inv(mat),y)
+    p=pars[:n]
+    q=pars[n:]
+    return p,q
+
+#1*p0 + x*p1 +x**2+p2+... -q1*x - q2*x**2... = y
