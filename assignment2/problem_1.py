005679810df8846bbf0290cddd01e95208c4abc4
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-09-21 19:43:31 -0400

    update

--- assignment2/problem_1.py
+++ assignment2/problem_1.py
@@ -2,6 +2,7 @@ import numpy as np
 from integrator import integrate_step #Old integrator
 from integrator import integrator #New integrator
 
+
 #Let's write out some common functions that we can integrate
 def Gauss(x, u=0, sig=1):
     return (1/np.sqrt(2*np.pi*sig**2))*np.exp((-(x-u)**2)/(2*sig**2))
@@ -13,4 +14,4 @@ def log(x):
     return np.log(x)
 
 #Just a check to see if the function is integrating correctly
-print(integrate_step(log,1,10,0.001), integrator(log,1,10,0.001))
+print(integrate_step(Gauss,1,5,0.01), integrator(Gauss,1,5,0.01))
