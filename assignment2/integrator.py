d259195e2cc9853019af2126acda03f3c809fe71
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-09-21 22:20:29 -0400

    update

--- assignment2/integrator.py
+++ assignment2/integrator.py
@@ -8,7 +8,6 @@ def integrate_step(fun,x1,x2,tol):
     area1=(x2-x1)*(y[0]+4*y[2]+y[4])/6
     area2=(x2-x1)*(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/12
     myerr=np.abs(area1-area2)
-    print("bas",y)
     if myerr<tol:
         return area2,n
     else:
@@ -17,38 +16,25 @@ def integrate_step(fun,x1,x2,tol):
         a2,n2=integrate_step(fun,xm,x2,tol/2)
         return a1+a2,n1+n2+n
 
-"""New Integrator: Using some new adaptation of simpsons method. If we study the previous integrator, we realise that each time the ytion is recursively called, the line y=fun(x) is repeated. This is the line we only want to run once. Essentially, the only thing that changes each time this ytion is called is x1 and x2, hence x changes. xm is the middle of both bounds and hence, we don't need to evaluate our ytion for 5 points each time it is called since we already know what the middle term is, and we also know what the ytion is at the bounds. We would just need to calculate the two intermediate points, i.e. point 1 and 3. Hence, we are not calling f(x) for the same x each time which would be 5 calculations, instead we are doing 2 calculations. To do this, we need a new variable n that stores the number of times the integrator has been called, so that if its been called already it does not calculate y=fun(x) again. We need to adjust the values of the bounds that we use for the ytion though.
+"""New Integrator: Using some new adaptation of simpsons method. If we study the previous integrator, we realise that each time the function is recursively called, the line y=fun(x) is repeated. This is the line we only want to run once. Essentially, the only thing that changes each time this function is called is x1 and x2, hence x changes. xm is the middle of both bounds and hence, we don't need to evaluate our function for 5 points each time it is called since we already know what the middle term is, and we also know what the function is at the bounds. We would just need to calculate the two intermediate points, i.e. point 1 and 3. Hence, we are not calling f(x) for the same x each time which would be 5 calculations, instead we are doing 2 calculations. To do this, we need a new variable n that stores the number of times the integrator has been called, so that if its been called already it does not calculate y=fun(x) again. We need to adjust the values of the bounds that we use for each recursion though. To do this, we need to make sure that the function stores the values of y[0],y[2] and y[4], especially when the first integrator returns, otherwise the second integrator will be run with the incorrect y-array. We can write these arguments into the function to store each time.
 """
-def integrator(f,x1,x2,tol,n=0,y=[],xm=0,sy=[]):
+def integrator(f,x1,x2,tol,n=0,y1=0,ym=0,y2=0):
     x = np.linspace(x1,x2,5)
-    print("sybeg", sy)
     if n == 0: #If has been evaluated 0 times, then it evaluates f(x)
         y = f(x)
+        y1,mid1,ym,mid2,y2 = y[0],y[1],y[2],y[3],y[4] #Defining all the variables so that the function works
         n = len(x)
-    else: #If has been evaluated >0 times, then it just calculates the two new values that it needs to.
-        if x1<xm: #Something wrong here, possibly the condition is wrong?
-            print(1,y,sy)
-            y[4] = y[2] #Last point is messed up for some reason
-            y[2] = y[1]
-        else:
-            print(2,y,sy)
-            y[0] = y[2]
-            y[2] = y[3]
-        y[1] = f(x[1]) #This is fine
-        y[3] = f(x[3]) #^
+    else: #If has been evaluated >0 times, then it just calculates the two new values that it needs to
+        mid1 = f(x[1]) #Re-calculating the two points that need to be calculated
+        mid2 = f(x[3])
         n = 2
-    area1 = (x2-x1)*(y[0]+4*y[2]+y[4])/6
-    area2 = (x2-x1)*(y[0]+4*y[1]+2*y[2]+4*y[3]+y[4])/12
+    area1 = (x2-x1)*(y1+4*ym+y2)/6
+    area2 = (x2-x1)*(y1+4*mid1+2*ym+4*mid2+y2)/12
     myerr = np.abs(area1-area2)
-    print("after",y,sy)
     if myerr < tol:
-        print("returning",y,sy)
-        y=sy
         return area2, n
     else:
         xm = 0.5*(x1+x2)
-        sy=y
-        print("sy", sy)
-        a1,n1 = integrator(f,x1,xm,tol/2,n,y,xm,sy)
-        a2,n2 = integrator(f,xm,x2,tol/2,n,y,xm,sy)
+        a1,n1 = integrator(f,x1,xm,tol/2,n,y1,mid1,ym) #Changing what we use as y1,ym and y2 as mentioned above
+        a2,n2 = integrator(f,xm,x2,tol/2,n,ym,mid2,y2)
         return a1+a2, n1+n2+n
--- assignment2/problem_1.py
+++ assignment2/problem_1.py
@@ -2,16 +2,25 @@ import numpy as np
 from integrator import integrate_step #Old integrator
 from integrator import integrator #New integrator
 
-
 #Let's write out some common functions that we can integrate
 def Gauss(x, u=0, sig=1):
     return (1/np.sqrt(2*np.pi*sig**2))*np.exp((-(x-u)**2)/(2*sig**2))
 
-def quad(x):
-    return x**2 + 2*x + 1
+def sqrt(x):
+    return np.sqrt(x)
+
+def abs(x):
+    return np.abs(x)
 
 def log(x):
-    return np.log(x)
+    return x*np.log(x)
 
-#Just a check to see if the function is integrating correctly
-print(integrate_step(Gauss,1,5,0.01), integrator(Gauss,1,5,0.01))
+def semi_circle(x,r=1):
+    return np.sqrt(r-x**2)
+
+#Comparing all these functions
+print("Gaussian number of reduced calls =", integrate_step(Gauss,-5,5,0.001)[1]-integrator(Gauss,-5,5,0.001)[1])
+print("√x number of reduced calls =", integrate_step(sqrt,0,5,0.001)[1]-integrator(sqrt,0,5,0.001)[1])
+print("|x| number of reduced calls =", integrate_step(sqrt,0,2,0.001)[1]-integrator(sqrt,0,2,0.001)[1])
+print("xln(x) number of reduced calls =", integrate_step(log,0.5,5,0.001)[1]-integrator(log,0.5,5,0.001)[1])
+print("Semi-circle of radius 1 number of reduced calls =", integrate_step(semi_circle,-1,1,0.001)[1]-integrator(semi_circle,-1,1,0.001)[1])
