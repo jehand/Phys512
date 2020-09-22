fda438c25c3062e2e662dce822ef3ddcc619d6fc
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-09-21 23:28:03 -0400

    update

--- assignment2/problem_2.py
+++ assignment2/problem_2.py
@@ -1,17 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from chebyshev import cheby
 
 #First we define our function
-x = np.linspace(0.5,1,50)
+x = np.linspace(0.5,1,1000)
 y = np.log2(x)
-plt.plot(x,y,label="$log_2{x}$")
 
-#Chebyshev polynomial fit
+ord=7
+#Chebyshev polynomial fit untruncated
+cy = cheby(x,y,ord)
+plt.plot(x,y-cy,label="Cheby")
 
+#Now we need to figure out how many terms we need: Do this by calculating accuracy for a 
 
 #Polynomial fit
-coeffs = np.polynomial.legendre.legfit(x,y,5) #Using the same degree
-ly = np.polynomial.legendre.legval(coeffs,x)
-plt.plot(x,ly,"label=Polynomial")
+coeffs = np.polynomial.legendre.legfit(x,y,ord) #Using the same degree
+ly = np.polynomial.legendre.legval(x,coeffs)
+plt.plot(x,y-ly,label="Polynomial")
+plt.legend()
 #plt.savefig("problem_2_plot.png", bbox_inches=True, dpi=500)
 plt.show()
