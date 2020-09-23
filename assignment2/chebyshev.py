ea4f2de3998d515faf2dbe4f5cb476281a495dc8
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-09-22 22:02:44 -0400

    update

--- assignment2/chebyshev.py
+++ assignment2/chebyshev.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 """
-We first recognise that the range [0.5,1] is not the range of the Chebyshev polynomials. Hence, we must adjust the range of our function. This can be done fairly easily by taking the range of x of our function i.e. from 0.5-1 and rescaling it to be -1,1. We do not have to do this generally, as we are only concerned with log_2(x) from 0.5-1, hence we shall do it specifically for this case. Essentially our x values are scaled up by 4 (since we're going from a range of 0.5 to a range of 2) and a factor of -3 is added such that the x values match up (since multiplying 0.5 by 4 gives 2 and we want it to be at -1). After this we must find the cheby matrix which we can take directly from the lectures. 
+We first recognise that the range [0.5,1] is not the range of the Chebyshev polynomials. Hence, we must adjust the range of our function. This can be done fairly easily by taking the range of x of our function i.e. from 0.5-1 and rescaling it to be -1,1. We do not have to do this generally, as we are only concerned with log_2(x) from 0.5-1, hence we shall do it specifically for this case. Essentially our x values are scaled up by 4 (since we're going from a range of 0.5 to a range of 2) and a factor of -3 is added such that the x values match up (since multiplying 0.5 by 4 gives 2 and we want it to be at -1). After this we must find the cheby matrix and do the fitting which we can take directly from the lectures. 
 """
 
-def cheby(x,y,ord):
+def cheby(x,y,ord,trunc):
     x = 4*x - 3 #Changing the range of our x-values to match
     nx = len(x)
     mat=np.zeros([nx,ord+1])
@@ -13,4 +13,4 @@ def cheby(x,y,ord):
     for i in range(1,ord):
         mat[:,i+1]=2*x*mat[:,i]-mat[:,i-1]
     coeffs = np.linalg.pinv(mat)@y
-    return np.polynomial.chebyshev.chebval(x,coeffs),coeffs
+    return np.polynomial.chebyshev.chebval(x,coeffs[:trunc+1])
--- assignment2/problem_2.py
+++ assignment2/problem_2.py
@@ -7,25 +7,33 @@ x = np.linspace(0.5,1,1000)
 y = np.log2(x)
 
 """
-Now we need to figure out how many terms we need. We do this by first arbitrarily picking a Chebyshev polynomial of order 20, and then using while loop to see when the max error no longer exceeds 10^-6. 
+Now we need to figure out how many terms we need for the truncated fit. We do this by first arbitrarily picking a Chebyshev polynomial of order 20, and then using while loop to see when the max error no longer exceeds 10^-6. 
 """
-n = 1
-ncheby = 20 #Arbitrarily chosen
-yerr = [1] #Just for the while loop to work properly and to update the value of yerr
-c = cheby(x,y,ncheby)[1] #Coefficients for the Cheby fit
 
-while max(np.abs(yerr))>=1e-6 and n<ncheby:
-    yerr = y-np.polynomial.chebyshev.chebval(x,c[:n+1]) #Calculating the error each time using n coefficients
-    n += 1
-print("truncated order=",n)
+ncheby = 20 #Arbitrarily chosen
+n = 1 #Truncated number
+c = cheby(x,y,ncheby,n) #Coefficients for the Cheby fit
+while max(np.abs(y-c))>=1e-6 and n<ncheby:
+    c = cheby(x,y,ncheby,n)
+    n+=1t
+n-=1
+print("Truncated Chebyshev Polynomial fit: truncated order =", n, "max error =", max(np.abs(y-c)), "RMS error =", np.std(y-c,ddof=1)) #ddof=1 for sample std.
 
 #Plotting the truncated fit
-plt.plot(x,yerr,"r-", label="Order " +str(ncheby)+ " Cheby truncated to order " + str(n))
+plt.plot(x,y-c,"r-", label="Order " +str(ncheby)+ " Cheby truncated to order " + str(n))
+
+#Plotting the untruncated chebyshev polynomial of order 7
+cuy = cheby(x,y,n,n)
+plt.plot(x,y-cuy, "k-", label="Cheby polynomial of order 7")
+print("Untruncated Chebyshev Polynomial of rder =", n, "max error =", max(np.abs(y-cuy)), "RMS error =", np.std(y-cuy, ddof=1)) #ddof=1 for sample std.
 
 #Polynomial fit
-coeffs = np.polynomial.legendre.legfit(x,y,7) #Using the same degree
+coeffs = np.polynomial.legendre.legfit(x,y,n) #Using the same degree
 ly = np.polynomial.legendre.legval(x,coeffs)
+print("Polynomial fit: max error =", max(np.abs(y-ly)), "RMS error =", np.std(y-ly,ddof=1)) #ddof=1 for sample std.
 plt.plot(x,y-ly,"--",label="Polynomial")
+plt.xlabel("x", fontsize=13)
+plt.ylabel("Residual", fontsize=13)
 plt.legend()
-#plt.savefig("problem_2_plot.png", bbox_inches=True, dpi=500)
+plt.savefig("problem_2_plot.png", bbox_inches="tight", dpi=500)
 plt.show()
