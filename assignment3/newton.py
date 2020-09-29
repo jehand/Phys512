477db33051db93e03899a235fcb1fae59d6813fb
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-09-28 23:36:39 -0400

    update

--- assignment3/newton.py
+++ assignment3/newton.py
@@ -1,5 +1,39 @@
 import numpy as np
+import camb
+from wmap_camb_example import get_spectrum
 
 """
-We take inspiration from the "newton.py" file used in lectures to write our own Newton's method best fit finder. We notice that 
+We take inspiration from the "newton.py" file used in lectures to write our own Newton's method best fit finder. The main difference is that we no
+longer have an estimate for the derivatives as CAMB does not give us derivatives with respect to the model. A simple solution to this would be to
+calculate that function at a point (x+∆x) where ∆x is small and to calculate the derivative by the usual method: f' = f(x+∆x)-f(x)/∆x. This is for
+taking the limit from the left side however, for both sides we also need to calculate f(x-∆x) therefore f' = f(x+∆x)-f(x-∆x)/2∆x. This is a large
+number of calculations! Besides this, we carry out the normal Newton's method as described in class. 
 """
+
+def newton(d,l,pars,pred,delx,N,tau=True): #tau=True when we already know the value of tau
+    r = d-pred #residual array
+    N = np.linalg.pinv(N) #calculating inverse N
+    #We next look to calculate all the derivatives
+    dA = [] #np.zeros(shape=(len(d),len(pars)))
+    for i in range(len(pars)): #Calculating the derivatives for each parameter
+        fxplus = get_spectrum(pars[i]+delx[i],l)
+        fxminus = get_spectrum(pars[i]-delx[i],l)
+        dA[:,i] = (fxplus-fxminus)/(2*delx[i])
+
+    #We now have our derivatives, however if we are not concerned with tau, we do not need that column of the matrix.
+    #dA = np.delete(dA,3,axis=1) if tau else dA
+    #Proceeding with Newton's since we now have everything
+    dm = np.linalg.pinv(dA.transpose() @N @dA) @ (dA.transpose() @N @r)
+
+    #If we already have the value for tau
+    dm = np.insert(dm,3,0,axis=0) if tau else dm
+    return pars+dm
+
+"""
+We now write our function that will cycle through values to give us the minimum value of 
+"""
+def newton_chi(d,l,pars,pred,delx,N,tau=True,chimin):
+    while delchi2>chimin:
+        newton = newton(d,l,pars,pred,delx,N,tau=True)
+
+    return chi2, 
--- assignment3/problem_3.py
+++ assignment3/problem_3.py
@@ -1,9 +1,16 @@
 import numpy as np
 from wmap_camb_example import get_spectrum
+from newton import newton
 
 d = np.loadtxt("wmap_tt_spectrum_9yr_v5.txt", skiprows=20, usecols=(0,1,2)) #Importing the data
 l, pow, err = d[:,0], d[:,1], d[:,2] #Splitting the data into its appropriate values
 
-#Our variables are written in the form [H0, ombh2, omch2, tau, As, ns]. We begin by writing a script that utilizes Newton's method to find the best
-#fit parameters for our code. This script can be found in "newton.py"
+"""Our variables are written in the form [H0, ombh2, omch2, tau, As, ns]. We begin by writing a script that utilizes Newton's method to find the best 
+fit parameters for our code. This script can be found in "newton.py". Our noise matrix is a matrix with err**2 on the diagonal. Our initial
+parameters are the parameters we used in problem2. 
+"""
+pars = [65,0.02,0.1,0.05,2e-9,0.96]
+dx = 0.001*pars #as the values of pars vary drastically, our dx value cannot be the same for all, hence arbitrarily is 0.1% of each parameter.
+N = np.diag(err**2)
+
 
