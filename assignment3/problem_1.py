61c12f3dd883a05c5a83ae57420e254d7419f3d8
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-10-04 23:51:25 -0400

    linear update

--- assignment3/problem_1.py
+++ assignment3/problem_1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import matplotlib as mpl
 
-cmap = mpl.cm.Greys(np.linspace(0,1,50))
+cmap = mpl.cm.Reds(np.linspace(0,1,50)) #Next 2 lines are just to make the 3D plot easier to see
 cmap = mpl.colors.ListedColormap(cmap[30:,:-1])
 
 data = np.loadtxt("dish_zenith.txt") #Importing data
@@ -12,34 +12,44 @@ data = np.loadtxt("dish_zenith.txt") #Importing data
 We now carry out our fit. First, we define our predicted parabaloid function that is linear in our new parameters. 
 """
 
-def para(x,y,x0,y0,c0,a):
-    return a(x**2+y**2-x0*x-y0*x)+c0 #returning z-values
+def para(x,y,x0,y0,c0,a1,a2):
+    return a1*x**2+a2*y**2+x0*x+y0*y+c0 #returning z-values
 
-"""def linear(d,pars):
-    vec = d[:,:2] #We want to just use the x,y columns.
-    A = np.empty(shape=(len(data),pars))
-    A[:,0] = 1.0
-    for i in range(1,pars):
-        A[:,i] = vec @ A[:,i-1]
+def para1(u,v,a,z0):
+    return a*(u**2+v**2)+z0
+
+def linear(d,vars): #vars is the number of variables we are fitting i.e. x^2, x, y^2, y
+    #vec = d[:,:2].reshape(len(d)*2) #We want to just use the x,y columns.
+    A = np.empty(shape=(len(d),vars+1)) #A = [1,x^2,x,y^2,y]
+    A[:,0], A[:,1], A[:,2],A[:,3], A[:,4] = 1.0, d[:,0]**2, d[:,0], d[:,1]**2, d[:,1]
     cov = np.linalg.inv(A.transpose() @ A)
-    m = cov @ (A.transpose() @ vec)
+    m = cov @ (A.transpose() @ d[:,2])
     return m
-
-pars = linear(data,4)"""
 
 #Plotting the original data in 3D
 x,y,z = data[:,0], data[:,1], data[:,2]
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
-ax.scatter3D(x,y,z,c=-z,s=10,cmap=cmap,label="Data")
+ax.scatter3D(x,y,z,c=-z,s=30,cmap=cmap,label="Data")
 
-#Now we create our surface using the our linear interpolation for z.
-"""
-X,Y = np.meshgrid(x,y)
-Z = para(X,Y,x0,y0,c0,a)
-ax.plot_trisurf(X,Y,Z,cmap="viridis",label="Linear Fit")
-"""
+#Now we create our surface using the our linear interpolation for z. We want to take x,y samples in a circle for best comparison
+c0,a1,x0,a2,y0 = linear(data,4)
+
+r = ((y.max()-y.min())+(x.max()-x.min()))/4 #Radius is just distance between xmin and xmax and ymin and ymax. Take average for best results.
+
+R = np.linspace(0,r,50)
+the = np.linspace(0,2*np.pi,50)
+
+xl = []
+yl = []
+for i in R:
+    for j in the:
+        xl.append(i*np.cos(j))
+        yl.append(i*np.sin(j))
+X,Y = np.meshgrid(xl,yl,sparse=True)
+Z = para(X,Y,x0,y0,c0,a1,a2)
+ax.plot_wireframe(X,Y,Z,color="grey",rcount=10,ccount=10,label="Linear Fit")
 
 ax.set_xlabel("x")
 ax.set_ylabel("y")
