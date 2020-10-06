14bf1b2ad9f67658767aa77ede81e980e381def1
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-10-05 23:33:04 -0400

    problem 4 update + plots folder

--- assignment3/markov.py
+++ assignment3/markov.py
@@ -4,24 +4,23 @@ from wmap_camb_example import get_spectrum
 
 """
 We can take the 'mcmc_class_wnewton.py' from lectures and adapt it for our needs. Print statements have been added just so that we are aware of where
-in the program we currently are.
+in the program we currently are. 
 """
 
 def mcmc(d,l,err,pars,cov,chi_cur,nstep=5000):
     print("Starting Markov")
     npar = len(pars)
-    n_success = 0
+    n_success = 0 #so we can determine acceptance rate
     r = np.linalg.cholesky(cov) #Calculating cholesky matrix
     chain = np.zeros([nstep,npar])
     chivec = np.zeros(nstep)
     for i in range(nstep):
         print("\t" + "Step #" + str(i+1))
-        par_step = np.dot(r,np.random.randn(r.shape[0]))*0.7 #Our step size is too large -> arbitrarily adjust by a factor of 0.4 (=35% success rate)
+        par_step = np.dot(r,np.random.randn(r.shape[0]))*0.7 #Our step size is too large -> arbitrarily adjust for each chain
         pars_trial = pars+par_step
         new_model = get_spectrum(pars_trial,l)
         chi_trial = np.sum((d-new_model)**2/(err**2))
-        #we now have chi^2 at our current location and chi^2 in our trial location. decide if we take the step
-        accept_prob = np.exp(-0.5*(chi_trial-chi_cur))
+        accept_prob = np.exp(-0.5*(chi_trial-chi_cur)) #decide if we take the step
         if (np.random.rand(1)<accept_prob) & (pars_trial[3]>0): #accept the step with appropriate probability + don't accept if tau<0. 
             pars = pars_trial
             chi_cur = chi_trial
--- assignment3/problem_4.py
+++ assignment3/problem_4.py
@@ -4,6 +4,7 @@ from wmap_camb_example import get_spectrum
 from newton import newton
 from newton import newton_chi
 from markov import mcmc
+from scipy import signal
 
 d = np.loadtxt("wmap_tt_spectrum_9yr_v5.txt", skiprows=20, usecols=(0,1,2)) #Importing the data
 l, pow, err = d[:,0], d[:,1], d[:,2] #Splitting the data into its appropriate values
@@ -17,36 +18,60 @@ dx = 0.001 #as the values of pars vary drastically, our dx value cannot be the s
 N = np.diag(err**2)
 
 newton = np.load("newton.npy", allow_pickle=True) #Let us load our Newton data from problem 3 for floating tau
-#print("Parameters =", newton[0], "\n"+"Errors =", np.sqrt(np.diag(newton[1])), "\n"+"Chi =", newton[2])
+
+#We now run our markov chain from 'markov.py' using the covariance given by Newtons method.
+n = 5000 #number of steps to use
+markov = mcmc(pow,l,err,newton[0],newton[1],newton[2],n)
 
 """
-We now run our markov chain from 'markov.py' using the covariance given by Newtons method.
-"""
-
-n = 5000 
-
-#markov = mcmc(pow,l,err,newton[0],newton[1],newton[2],n)
-#markov = mcmc(pow,l,err,parsi,newton[1],1588.42568,n)
-"""
+#For our next chains we use the covariance matrix of the previous chain in order to determine our step size
 chain0 = np.load("markov.npy", allow_pickle=True)
-chain0[0] = chain0[0][400:,];chain0[1] = chain0[1][400:,]
+chain0[0] = chain0[0][500:,];chain0[1] = chain0[1][500:,]
 parsc = np.mean(chain0[0], axis=0)
 cov = np.cov(chain0[0].transpose())
-markov = mcmc(pow,l,err,parsc,cov,chain0[1].mean(),n)"""
+markov = mcmc(pow,l,err,parsc,cov,chain0[1].mean(),n)
 
 chain1 = np.load("markov1.npy", allow_pickle=True)
 chain1[0] = chain1[0][4000:,];chain1[1] = chain1[1][4000:,]
 parsc1 = np.mean(chain1[0], axis=0)
 cov1 = np.cov(chain1[0].transpose())
 markov1 = mcmc(pow,l,err,parsc1,cov1,chain1[1].mean(),n)
-
 np.save("markov3", markov1)
-print(markov1)
-plt.plot(np.linspace(1,n,n),markov1[0][:,0],label="H0")
-plt.plot(np.linspace(1,n,n),markov1[0][:,1],label="wh")
-plt.plot(np.linspace(1,n,n),markov1[0][:,2],label="wc")
-plt.plot(np.linspace(1,n,n),markov1[0][:,3],label="tau")
-plt.plot(np.linspace(1,n,n),markov1[0][:,4],label="As")
-plt.plot(np.linspace(1,n,n),markov1[0][:,5],label="slope")
-plt.legend()
+print(markov1)"""
+
+#We load in our previously saved results
+chain = np.load("markov.npy",allow_pickle=True)
+chain1 = np.load("markov1.npy",allow_pickle=True)
+chain2 = np.load("markov2.npy",allow_pickle=True)
+
+burnindex = 100 #the determined burn in index for each chain, chain1=500, chain2=4000, chain3=100.
+n = len(chain[1][burnindex:])
+plt.plot(np.linspace(1,n,n),chain[1][burnindex:],label="H0",color="mediumblue")
+plt.xlabel("Step #", fontsize=14)
+plt.ylabel(r"$\chi^2$", fontsize=14)
+#plt.savefig("problem_4_chain3.png", bbox_inches="tight", dpi=500)
 plt.show()
+
+#We can now calculate the Fourier transform for each of our chains to see if we have reached convergence and determine independent samples
+chainfour = signal.periodogram(chain[1][burnindex:],scaling="spectrum",fs=2) #fs=2 to scale the fourier transform so that its max=1
+plt.plot(chainfour[0],chainfour[1],color="crimson")
+plt.ylim(1e-8,10) #Arbitrarily set to allow for better viewing
+plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("Sample Frequencies Scaled", fontsize=14)
+plt.ylabel("Power Spectrum", fontsize=14)
+#plt.savefig("problem_4_chain3fourier.png", bbox_inches="tight", dpi=500)
+plt.show()
+
+#Qualitative independent samples calculation
+chain1samples = 8000*0.025
+chain2samples = 4900*0.021
+
+#Calculating parameter values and errors
+chain1pars = np.mean(chain1[0][4000:,:],axis=0)
+chain1errs = np.std(chain1[0][4000:,:],ddof=1,axis=0)/np.sqrt(chain1samples)
+chain2pars = np.mean(chain2[0][100:,],axis=0)
+chain2errs = np.std(chain2[0][100:,],ddof=1,axis=0)/np.sqrt(chain2samples)
+pars = (chain1pars+chain2pars)/2 #Calculating pars by taking an average
+parserrs = np.sqrt(chain1errs**2+chain2errs**2)/2 #Calculating error in pars by taking the average
+print("pars=",pars,"\nerrs=",parserrs)
