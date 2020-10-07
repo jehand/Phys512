99a80a246147c60faaf845640aca795355d58be6
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-10-06 23:46:06 -0400

    update

--- assignment3/markov.py
+++ assignment3/markov.py
@@ -5,10 +5,12 @@ from wmap_camb_example import get_spectrum
 """
 We can take the 'mcmc_class_wnewton.py' from lectures and adapt it for our needs. Print statements have been added just so that we are aware of where
 in the program we currently are. Furthermore, we add a parameter tau_interv that checks whether tau has been given as a prior or not. If it has, it
-steps tau by the random sampling of a normal distribution centered at 0 with sig=1, multiplyed by our interval (i.e. tau's error).
+steps tau by the random sampling of a normal distribution centered at 0 with sig=1, multiplyed by our interval (i.e. tau's error). We also have a
+parameter tau that sets the mean value of tau, so each time we are iterating about the mean of tau, and hence our chain does not drag too far away
+from the origin.
 """
 
-def mcmc(d,l,err,pars,cov,chi_cur,nstep=5000,tau_interv=0):
+def mcmc(d,l,err,pars,cov,chi_cur,nstep=5000,tau=0,tau_interv=0):
     print("Starting Markov")
     npar = len(pars)
     n_success = 0 #so we can determine acceptance rate
@@ -18,9 +20,11 @@ def mcmc(d,l,err,pars,cov,chi_cur,nstep=5000,tau_interv=0):
     for i in range(nstep):
         print("\t" + "Step #" + str(i+1))
         par_step = np.dot(r,np.random.randn(r.shape[0]))
+        pars_trial = pars.copy()
         if tau_interv !=0: #If it is not the default, then we know our covariance matrix if pars-1 dimensional so we add another column for tau.
-            par_step = np.insert(par_step,3,tau_interv*np.random.randn())
-        pars_trial = pars+par_step*0.7 #Our step size is too large -> arbitrarily adjust for each chain
+            par_step = np.insert(par_step,3,tau_interv*np.random.randn()) #tau uncert is a 1sig unertainty, hence we step with a normal distribution.
+            pars_trial[3] = tau #Always setting tau to be what was determined by Planck and then taking a step from there.
+        pars_trial += par_step*0.5 #Our step size is too large -> arbitrarily adjust for each chain
         new_model = get_spectrum(pars_trial,l)
         chi_trial = np.sum((d-new_model)**2/(err**2))
         accept_prob = np.exp(-0.5*(chi_trial-chi_cur)) #decide if we take the step
--- assignment3/problem_5.py
+++ assignment3/problem_5.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from wmap_camb_example import get_spectrum
 from markov import mcmc
+from scipy import signal
 
 d = np.loadtxt("wmap_tt_spectrum_9yr_v5.txt", skiprows=20, usecols=(0,1,2)) #Importing the data
 l, pow, err = d[:,0], d[:,1], d[:,2] #Splitting the data into its appropriate values
@@ -13,10 +14,35 @@ steps. We then add our interval of tau as a prior using a random number fit.
 """
 
 #Importing newton data
-newton = np.load("newtontauconst.npy")
+newton = np.load("newtontauconst.npy", allow_pickle=True)
 
-n = 50 #number of steps
+n = 20000 #number of steps
 tau = 0.0544
 utau = 0.0073
+newton[0][3] = tau
+print("Initial Pars=", newton[0])
 
-markov = mcmc(pow,l,err,newton[0],newton[1],newton[2],n,utau)
+#We also wanna recalculate chi^2 as we now have new pars
+chi = np.sum((pow-get_spectrum(newton[0],l))**2/err**2)
+print("Starting Chi^2=", chi)
+
+markov = mcmc(pow,l,err,newton[0],newton[1],chi,n,tau,utau)
+np.save("markovtau2",markov)
+
+print("Tau avg=", np.mean(markov[0][:,3]))
+print("Acceptance Rate=",markov[-1])
+plt.plot(np.linspace(1,n,n),markov[0][:,0],label="H0")
+plt.plot(np.linspace(1,n,n),markov[0][:,3],label="Tau")
+plt.legend()
+plt.show()
+
+#We can also calculate the Fourier transform to see if we have reached convergence and determine independent samples
+chainfour = signal.periodogram(markov[1],scaling="spectrum",fs=2) #fs=2 to scale the fourier transform so that its max=1
+plt.plot(chainfour[0],chainfour[1],color="crimson")
+plt.ylim(1e-8,10) #Arbitrarily set to allow for better viewing
+plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("Sample Frequencies Scaled", fontsize=14)
+plt.ylabel("Power Spectrum", fontsize=14)
+#plt.savefig("problem_5_fourier.png", bbox_inches="tight", dpi=500)
+plt.show()
