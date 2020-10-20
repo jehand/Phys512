ab00fa3258b4fa1cea839af733bf4b7e9a3f126c
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-10-19 21:00:50 -0400

    problem 2 final update

--- assignment4/problem_2.py
+++ assignment4/problem_2.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 def corr(arr1,arr2):
     fft1 = fft(arr1)
     fft2 = fft(arr2)
-    return np.real(ifft(fft1*np.conj(fft2)))
+    return ifft(fft1*np.conj(fft2))
 
 x = np.linspace(-5,5,100)
 y = (1/np.sqrt(2*np.pi))*np.exp(-x**2/2) #We define a Gaussian centered at 0 with sigma=1.
 
-plt.plot(x,corr(y,y),color="teal",label="Correlation function")
+plt.plot(x,np.real(corr(y,y)),color="teal",label="Correlation function")
 plt.xlabel("x",fontsize=14)
 plt.ylabel("Correlation Function",fontsize=14)
 plt.savefig("problem_2.png",bbox_inches="tight",dpi=500)
