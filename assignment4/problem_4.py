854111e042b695fa61f2ce6c7bc4281bf348010a
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-10-22 19:43:14 -0400

    update

--- assignment4/problem_4.py
+++ assignment4/problem_4.py
@@ -2,10 +2,12 @@ import numpy as np
 from numpy.fft import fft,ifft
 import matplotlib.pyplot as plt
 
-#To get past the wrapping around nature of a Fourier transform, we use zero padding.
+#To get past the wrapping around nature of a Fourier transform, we use zero padding. Assume both arrays are the same length.
 def conv(arr1,arr2):
     arr1pad, arr2pad = np.pad(arr1,[0,len(arr1)]), np.pad(arr2,[0,len(arr2)])
     f1 = fft(arr1pad)
     f2 = fft(arr2pad)
     transform = ifft(f1*f2)
     return transform[:len(arr1)]
+
+#Let's take an example to check if this works. We take the convolution of a square wave with a Gaussian ~ N(0,1).
--- assignment4/problem_5.py
+++ assignment4/problem_5.py
@@ -1,31 +1,71 @@
 import numpy as np
-from numpy.fft import fft
+from numpy.fft import fft,fftfreq
 import matplotlib.pyplot as plt
 
-#We arbitrarly choose a non-integer sine wave of f=0.8
-f = 0.8
-x = np.linspace(-5,5,1000)
+#We arbitrarly choose a non-integer sine wave of f=7.7
+f = 7.7
+N = 1024 #arbitrarily chosen number of points
+x = np.linspace(-5,10,N) #non-integer number of periods
 y = np.sin(2*np.pi*f*x)
-print(y)
 
-#Determining the fft from numpy
-fftsine = fft(y)
-plt.plot(np.abs(fftsine))
+#Determining the fft from numpy (for all the following fft's we cut out the negative portion of the transform as it is symmetric anyways)
+fftsine = fft(y)[:int(len(y)/2)]
+freq = fftfreq(x.shape[-1])[:int(len(y)/2)]/((x.max()-x.min())/len(x))
+plt.plot(freq,np.abs(fftsine),"r-",label="FFT",linewidth=2)
 
-#Analytic estimate of the dft
-N = len(y)
-k = f
-analytic = (1-np.exp(-2*np.pi*complex(0,1)*k))/(1-np.exp(-2*np.pi*complex(0,1)*k/N))
-plt.plot(np.abs(y*analytic))
+#Analytic estimate of the DFT
+M = np.empty(shape=(N,N),dtype=complex)
+for i in range(0,N):
+    for j in range(0,N):
+        M[j,i] = np.exp(-2*np.pi*complex(0,1)*i*j/N)
+fftanalytic = M@y
+plt.plot(freq,np.abs(fftanalytic)[:int(len(y)/2)],"b--",label="Analytic",linewidth=2)
+plt.xlabel("Frequency (Hz)",fontsize=12)
+plt.ylabel("Amplitude",fontsize=12)
+plt.legend(fontsize=12)
+#plt.savefig("problem_5_analytic.png",bbox_inches="tight",dpi=500)
 plt.show()
 
-#Windowing: choose the window function to be 0.5-0.5cos(2πx/N)
-ywind = 0.5-0.5*np.cos(2*np.pi*x/N)
-ynew = y*ywind
-fftnew = fft(ynew)
-plt.plot(np.abs(fftnew))
+#Plot Residuals of the fit
+res = np.abs(fftanalytic)[:int(len(y)/2)]-np.abs(fftsine)
+plt.plot(freq,res*(1e11),color="darkblue")
+plt.xlabel("Frequency (Hz)",fontsize=12)
+plt.ylabel(r"Residauls ($10^{-11}$)",fontsize=12)
+#plt.savefig("problem_5_analyticres.png",bbox_inches="tight",dpi=500)
+plt.show()
+
+print("Mean Residual=",np.mean(res))
+print("Residual STD=",np.std(res,ddof=1))
+
+#Windowing: choose the window function to be 0.5-0.5cos(2πx/N) (i.e. the Hann window)
+ywind = np.hanning(N) #numpy function for the Hann window
+fftnew = fft(y*ywind)[:int(len(y)/2)]
+plt.plot(freq,np.abs(fftnew),color="darkblue",linewidth=2,label="With Hann Window")
+plt.plot(freq,np.abs(fftsine),"r-",linewidth=2,label="Without Hann Window")
+plt.xlabel("Frequency (Hz)",fontsize=12)
+plt.ylabel("Amplitude",fontsize=12)
+plt.legend(fontsize=12)
+#plt.savefig("problem_5_Hann.png",bbox_inches="tight",dpi=500)
+plt.show()
+
+#Plotting a log plot to compare the sine wave without window to the function with a window
+plt.semilogy(freq,np.abs(fftnew),color="darkblue",label="With Hann Window")
+plt.semilogy(freq,np.abs(fftsine),"r-",label="Without Hann Window")
+plt.xlabel("Frequency (Hz)",fontsize=12)
+plt.ylabel("Amplitude",fontsize=12)
+plt.legend(fontsize=12)
+#plt.savefig("problem_5_Hannlog.png",bbox_inches="tight",dpi=500)
 plt.show()
 
 #Fourier transform of the window
+ywind = np.hanning(N+1)
 ywindfft = fft(ywind)
+
+def wind(k):
+    first = 0.5*(np.sin(np.pi*N*k))/np.sin(np.pi*k)
+    second = 0.25*np.exp(-complex(0,1)*np.pi/N)*(np.sin(np.pi*N*(k-(1/N))))/np.sin(np.pi*(k-(1/N)))
+    third = 0.25*np.exp(complex(0,1)*np.pi/N)*(np.sin(np.pi*N*(k+(1/N))))/np.sin(np.pi*(k+(1/N)))
+    return np.exp(-complex(0,1)*np.pi*k*(N-1))*(first+second+third)
+
+print(np.abs(wind(np.linspace(0,N-1,N)/N))/N)
 print("Showing the Fourier Transform of the Window=",np.abs(ywindfft)/N)
