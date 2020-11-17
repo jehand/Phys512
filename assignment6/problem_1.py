ae7039314468b41cd930035b26a19131a8d51eb5
Jehan Dastoor <jehan.dastoor@mail.mcgill.ca>
2020-11-16 20:12:46 -0500

    assignment 6 update

--- assignment6/problem_1.py
+++ assignment6/problem_1.py
@@ -1,2 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+
+x,y,z = np.loadtxt("rand_points.txt",delimiter=" ",unpack=True) #Importing the data
+
+fig = plt.figure()
+ax = plt.axes(projection="3d") 
+ax.scatter3D(x,y,z,c=z,s=15)
+
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+plt.show()
